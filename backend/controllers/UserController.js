ff3539dbd71db6e25d0621bd551d1c8089f24386
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-10-19 23:47:25 -0300

    Endpoint get user by id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -127,10 +127,31 @@ const update = async (req, res) => {
     res.status(200).json(user);
 }
 
+//Get user by id
+const getUserById = async (req, res) => {
+
+    const { id } = req.params
+
+    try {
+        const user = await User.findById(id).select("-password");
+        if (!user) {
+            res.status(404).json({ errors: ["Usuário não encontrado"] })
+            return;
+        }
+    } catch (error) {
+        res.status(404).json({ errors: ["Id não encontrado"] });
+        return
+    }
+
+    res.status(200).json(user);
+
+}
+
 
 module.exports = {
     register,
     login,
     getCurrentUser,
-    update
+    update,
+    getUserById
 }
