878f793ff3d290f8212522762ce2f79c059b40c6
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-10-20 00:14:49 -0300

    endpoint insert photo

---
+++ backend/routes/PhotoRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+//Controllers
+const {insertPhoto} = require("../controllers/PhotoController")
+
+//Middlewares
+const {photoinsertValidation} = require("../middlewares/photoValidation");
+const authGuard = require("../middlewares/authGuard");
+const validate = require("../middlewares/handleValidation");
+const { imageUpload } = require("../middlewares/imageUpload");
+
+//Routes
+router.post("/",authGuard, imageUpload.single("image"),photoinsertValidation(),validate, insertPhoto);
+
+module.exports = router;
