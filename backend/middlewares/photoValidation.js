5a4cce59a9be384eb601e8dac3988160d69babd4
Cristian <cristian.santos0308@gmail.com>
2024-10-21 15:17:54 -0300

    Endpoint para comentários em fotos

--- backend/middlewares/photoValidation.js
+++ backend/middlewares/photoValidation.js
@@ -33,7 +33,16 @@ const photoUpdateValidation = () => {
     ]
 }
 
+const commentValidation = () => {
+    return [
+        body("comment")
+        .isString()
+        .withMessage("O comentário é obrigatório.")
+    ]
+}
+
 module.exports = {
     photoinsertValidation,
-    photoUpdateValidation
+    photoUpdateValidation,
+    commentValidation
 }
