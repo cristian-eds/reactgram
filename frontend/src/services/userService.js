7346645b3e5161ae2510bc26da6763d1f42998e8
Cristian <cristian.santos0308@gmail.com>
2024-10-23 16:05:29 -0300

    Criacao service buscar usuario pelo id

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -34,10 +34,27 @@ const updateProfile = async (data, token) => {
     }
 }
 
+// get user details by id
+const getUserDetails = async (id) => {
+
+    const config = requestConfig("GET");
+
+    try {
+
+        const res = await fetch(api + "/users/" + id, config)
+            .then(res => res.json())
+            .catch(err => err);
+
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 const userService = {
     profile,
-    updateProfile
+    updateProfile,
+    getUserDetails
 }
 
 export default userService;
