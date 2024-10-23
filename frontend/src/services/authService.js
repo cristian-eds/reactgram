6cb5d5abecb72563d9fe9719b37209cc514ba68d
Cristian <cristian.santos0308@gmail.com>
2024-10-23 10:02:35 -0300

    Criacao funcao login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -26,9 +26,31 @@ const logout = () => {
     localStorage.removeItem("user");
 }
 
+// login an user
+const login = async (user) => {
+    
+    const config = requestConfig("POST",user);
+
+    try {
+        const res = fetch(api+"/users/login", config)
+                    .then((res) => res.json())
+                    .catch((err) => err);
+
+        if(res) {
+            localStorage.setItem("user", JSON.stringify(res));
+        }
+
+        return res;
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const authService = {
     register,
-    logout
+    logout,
+    login
 }
 
 export default authService;
