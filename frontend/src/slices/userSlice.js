33a0d77c2585ad0620cf414b0a18220f6739edcf
Cristian <cristian.santos0308@gmail.com>
2024-10-23 10:56:00 -0300

    Funcao no slice buscar detalhes do usuario

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -1,14 +1,26 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import userService from "../services/userService";
 
 const initialState = {
-    user:{},
-    error:false,
-    succes:false,
-    loading:false,
-    message:null
+    user: {},
+    error: false,
+    success: false,
+    loading: false,
+    message: null
 }
 
-//funcoes
+//get user details
+export const profile = createAsyncThunk("user/profile",
+    async (user, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await userService.profile(user, token);
+
+        return data;
+
+    }
+)
 
 export const userSlice = createSlice({
     name: "user",
@@ -17,8 +29,20 @@ export const userSlice = createSlice({
         resetMessage: (state) => {
             state.message = null;
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(profile.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        })
+            .addCase(profile.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.user = action.payload;
+            })
     }
 })
 
-export const {resetMessage} = userSlice.actions;
+export const { resetMessage } = userSlice.actions;
 export default userSlice.reducer;
