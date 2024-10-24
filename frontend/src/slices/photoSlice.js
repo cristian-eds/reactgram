0bc1f0bfc6fbb5c5b39231baabcca157ed9b902c
Cristian <cristian.santos0308@gmail.com>
2024-10-24 16:52:32 -0300

    Slice get user photos

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -27,6 +27,20 @@ export const publishPhoto = createAsyncThunk(
     }
 )
 
+//  get user photos
+export const getUserPhotos = createAsyncThunk(
+    "photo/userphotos",
+    async (id,thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await photoService.getUserPhotos(id,token);
+
+        return data;
+        
+    }
+)
+
 
 export const photoSlice = createSlice({
     name: "photo",
@@ -54,8 +68,17 @@ export const photoSlice = createSlice({
             state.error = action.payload;
             state.photo = {};
         })
+        .addCase(getUserPhotos.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        })
+        .addCase(getUserPhotos.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photos = action.payload; 
+        })
     }
-    
 });
 
 export const { resetMessage } = photoSlice.actions;
