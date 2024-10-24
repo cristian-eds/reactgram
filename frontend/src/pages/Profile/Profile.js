35aafe21bc36468bab9cc072e819b3fca160288d
Cristian <cristian.santos0308@gmail.com>
2024-10-24 16:59:59 -0300

    Estrutura inicial da exibicao das fotos para usuario

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -17,7 +17,7 @@ import { useParams } from 'react-router-dom';
 
 //redux
 import { getUserDetails } from '../../slices/userSlice';
-import { publishPhoto, resetMessage } from '../../slices/photoSlice';
+import { publishPhoto, resetMessage, getUserPhotos } from '../../slices/photoSlice';
 
 const Profile = () => {
 
@@ -46,6 +46,7 @@ const Profile = () => {
     // load user data
     useEffect(() => {
         dispatch(getUserDetails(id));
+        dispatch(getUserPhotos(id));
     }, [dispatch, id])
 
 
@@ -116,6 +117,22 @@ const Profile = () => {
                     {messagePhoto && <Message msg={messagePhoto} type="success"></Message>}
                 </>
             )}
+            <div className="user-photos">
+                <h2>Fotos publicadas</h2>
+                <div className="photos__container">
+                    {photos && photos.map(photo => (
+                        <div className="photo" key={photo._id}>
+                            {photo.image && (
+                                <img src={`${uploads}/photos/${photo.image}`} alt={photo.title} />
+                            )}
+                            {id === userAuth._id ? (
+                                <p>actions</p>
+                            ) : ( <Link className="btn" to={`/photos/${photo._id}`}>Ver</Link> ) }
+                        </div>
+                    ))}
+                    {photos.length === 0 &&  <p>Ainda não há fotos publicadas.</p>  }
+                </div>
+            </div>
         </div>
     )
 }
