b8abf8cbffd56dde7420bd1642149cd1bd45e209
Cristian <cristian.santos0308@gmail.com>
2024-10-24 14:40:33 -0300

    Slice para insercao de foto

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import userService from "../services/userService";
+import photoService from "../services/photoService";
 
 const initialState = {
     photos: [],
@@ -11,7 +12,21 @@ const initialState = {
 }
 
 
-// funcoes
+// publish user photo
+export const publishPhoto = createAsyncThunk(
+    "photo/publish",
+    async (photo, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await photoService.publishPhoto(photo,token);
+
+        // check for errors
+        if(data.errors) return thunkAPI.rejectWithValue(data.errors[0]);
+
+        return data
+    }
+)
 
 
 export const photoSlice = createSlice({
@@ -21,6 +36,25 @@ export const photoSlice = createSlice({
         resetMessage: (state) => {
             state.message = null;
         },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(publishPhoto.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        })
+        .addCase(publishPhoto.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photo = action.payload;
+            state.photos.unshift(state.photo);
+            state.message = "Foto publicada com sucesso!"
+        })
+        .addCase(publishPhoto.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.photo = {};
+        })
     }
     
 });
