79032a3ef96b597705822b85a89c34818b74496a
Cristian <cristian.santos0308@gmail.com>
2024-10-25 17:14:43 -0300

    Slice buscar todas as fotos

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -117,6 +117,15 @@ export const comment = createAsyncThunk(
 
     }
 )
+ //get all photos
+export const getPhotos = createAsyncThunk(
+    "photo/getAll", 
+    async () => {
+        const data = await photoService.getPhotos();
+
+        return data;
+    }
+)
 
 export const photoSlice = createSlice({
     name: "photo",
@@ -232,6 +241,16 @@ export const photoSlice = createSlice({
             .addCase(comment.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload;
+            })
+             .addCase(getPhotos.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(getPhotos.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photos = action.payload;
             })
     }
 });
