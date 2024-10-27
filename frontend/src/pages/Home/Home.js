d876e845850a4a94e7d3e43a2b4511c840ab4e5b
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-10-27 20:26:51 -0300

    Finalizado pagina Home e estilizacoes

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
 
   const dispatch = useDispatch();
 
-  const resetMessage = useResetComponentMessage();
+  const resetMessage = useResetComponentMessage(dispatch);
 
   const { user } = useSelector(state => state.auth);
   const { photos, loading } = useSelector(state => state.photo);
@@ -38,7 +38,20 @@ const Home = () => {
   }
 
   return (
-    <div>Home</div>
+    <div id="home">
+      {photos && photos.map(photo=> (
+        <div key={photo._id}>
+          <PhotItem photo={photo}></PhotItem>
+          <LikeContainer photo={photo} user={user} handleLike={handleLike}></LikeContainer>
+          <Link className='btn' to={`/photos/${photo._id}`}>Ver mais</Link>
+        </div>  
+      ))}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          Ainda não há fotos publicadas, <Link to={`/users/${user._id}`}>clique aqui</Link>
+        </h2>
+      )}
+    </div>
   )
 }
 
